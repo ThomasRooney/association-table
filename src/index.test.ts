171781a2b24bbb096ded19f6131e55d243a4cdae
Thomas Rooney <thomas.c.rooney@gmail.com>
2018-05-14 00:31:32 +0100

    Initial implementation

---
+++ src/index.test.ts
@@ -0,0 +1,239 @@
+import AssociationTable from "./index";
+
+describe("empty map", () => {
+  const map = new AssociationTable();
+  it("size", () => {
+    expect(map.size).toBe(0);
+  });
+  it("clear", () => {
+    map.clear();
+    expect(map.size).toBe(0);
+  });
+  it("inspect", () => {
+    expect(map.inspect()).toBe("AssociationTable {}");
+  });
+});
+
+describe('single item {"A": 1}', () => {
+  const map = new AssociationTable<string, number>();
+  beforeEach(() => {
+    map.clear();
+    map.associate("A", 1);
+  });
+  it("inspect", () => {
+    expect(map.inspect()).toBe("AssociationTable {A => 1}");
+  });
+  it("size", () => {
+    expect(map.size).toBe(1);
+  });
+  it("get", () => {
+    expect(map.get("A")).toMatchObject([1]);
+  });
+  it("getKey", () => {
+    expect(map.getKey(1)).toMatchObject(["A"]);
+  });
+  it("getValue", () => {
+    expect(map.getValue("A")).toMatchObject([1]);
+  });
+  it("associate", () => {
+    map.associate("A", 2);
+    expect(map.get("A")).toMatchObject([1, 2]);
+  });
+  it("clear", () => {
+    map.clear();
+    expect(map.size).toBe(0);
+    expect(map.get("A")).toMatchObject([]);
+  });
+  it("delete", () => {
+    map.delete("A");
+    expect(map.size).toBe(0);
+    expect(map.get("A")).toMatchObject([]);
+  });
+  it("deleteKey", () => {
+    map.deleteKey("A");
+    expect(map.size).toBe(0);
+    expect(map.get("A")).toMatchObject([]);
+  });
+  it("deleteValue", () => {
+    map.deleteValue(1);
+    expect(map.size).toBe(0);
+    expect(map.get("A")).toMatchObject([]);
+  });
+  it("forEach", () => {
+    let iterations = 0;
+    map.forEach((value, key, mapRef) => {
+      iterations++;
+      expect(key).toBe("A");
+      expect(value).toBe(1);
+      expect(mapRef.get(key)).toMatchObject([1]);
+    });
+    expect(iterations).toBe(map.size);
+  });
+  it("has", () => {
+    expect(map.has("A")).toBe(true);
+    expect(map.has("B")).toBe(false);
+  });
+  it("hasKey", () => {
+    expect(map.hasKey("A")).toBe(true);
+    expect(map.hasKey("B")).toBe(false);
+  });
+  it("hasValue", () => {
+    expect(map.hasValue(1)).toBe(true);
+    expect(map.hasValue(2)).toBe(false);
+  });
+});
+
+describe("alphabet letter => index", () => {
+  const map = new AssociationTable<string, number>();
+  beforeEach(() => {
+    for (let i = 1; i <= 26; i++) {
+      map.associate(String.fromCharCode(i + 64), i);
+    }
+  });
+
+  it("size", () => {
+    expect(map.size).toBe(26);
+  });
+  it("inspect", () => {
+    expect(map.inspect()).toBe(
+      "AssociationTable {" +
+        "A => 1, B => 2, C => 3, D => 4, E => 5, " +
+        "F => 6, G => 7, H => 8, I => 9, J => 10, " +
+        "K => 11, L => 12, M => 13, N => 14, O => 15, " +
+        "P => 16, Q => 17, R => 18, S => 19, T => 20, " +
+        "U => 21, V => 22, W => 23, X => 24, Y => 25, " +
+        "Z => 26}"
+    );
+  });
+  it("get", () => {
+    for (let i = 1; i <= 26; i++) {
+      expect(map.get(String.fromCharCode(i + 64))).toMatchObject([i]);
+    }
+  });
+  it("getKey", () => {
+    for (let i = 1; i <= 26; i++) {
+      expect(map.getKey(i)).toMatchObject([String.fromCharCode(i + 64)]);
+    }
+  });
+  it("getValue", () => {
+    for (let i = 1; i <= 26; i++) {
+      expect(map.getValue(String.fromCharCode(i + 64))).toMatchObject([i]);
+    }
+  });
+  it("associate", () => {
+    for (let i = 1; i <= 26; i++) {
+      map.associate(String.fromCharCode(i + 96), i);
+    }
+    expect(map.inspect()).toBe(
+      "AssociationTable {" +
+        "A => 1, B => 2, C => 3, D => 4, E => 5, " +
+        "F => 6, G => 7, H => 8, I => 9, J => 10, " +
+        "K => 11, L => 12, M => 13, N => 14, O => 15, " +
+        "P => 16, Q => 17, R => 18, S => 19, T => 20, " +
+        "U => 21, V => 22, W => 23, X => 24, Y => 25, " +
+        "Z => 26, a => 1, b => 2, c => 3, d => 4, e => 5, " +
+        "f => 6, g => 7, h => 8, i => 9, j => 10, k => 11, " +
+        "l => 12, m => 13, n => 14, o => 15, p => 16, q => 17, " +
+        "r => 18, s => 19, t => 20, u => 21, v => 22, w => 23, " +
+        "x => 24, y => 25, z => 26}"
+    );
+  });
+
+  it("clear", () => {
+    map.clear();
+    expect(map.size).toBe(0);
+    for (let i = 1; i <= 26; i++) {
+      expect(map.get(String.fromCharCode(i + 64))).toMatchObject([]);
+    }
+  });
+  it("delete", () => {
+    for (let i = 1; i <= 26; i++) {
+      map.delete(String.fromCharCode(i + 64));
+      expect(map.get(String.fromCharCode(i + 64))).toMatchObject([]);
+    }
+    expect(map.size).toBe(0);
+  });
+  it("deleteKey", () => {
+    for (let i = 1; i <= 26; i++) {
+      map.deleteKey(String.fromCharCode(i + 64));
+      expect(map.get(String.fromCharCode(i + 64))).toMatchObject([]);
+    }
+    expect(map.size).toBe(0);
+  });
+  it("deleteValue", () => {
+    for (let i = 1; i <= 26; i++) {
+      map.deleteValue(i);
+      expect(map.get(String.fromCharCode(i + 64))).toMatchObject([]);
+    }
+    expect(map.size).toBe(0);
+  });
+  it("forEach", () => {
+    let iterations = 0;
+    map.forEach((value, key, mapRef) => {
+      iterations++;
+      expect(map.get(key)).toMatchObject([value]);
+      expect(map.getKey(value)).toMatchObject([key]);
+      expect(mapRef.get(key)).toMatchObject([value]);
+    });
+    expect(iterations).toBe(map.size);
+  });
+  it("has", () => {
+    for (let i = 1; i <= 26; i++) {
+      expect(map.has(String.fromCharCode(i + 64))).toBe(true); // A-Z
+      expect(map.has(String.fromCharCode(i + 96))).toBe(false); // a-z
+    }
+  });
+  it("hasKey", () => {
+    for (let i = 1; i <= 26; i++) {
+      expect(map.hasKey(String.fromCharCode(i + 64))).toBe(true); // A-Z
+      expect(map.hasKey(String.fromCharCode(i + 96))).toBe(false); // a-z
+    }
+  });
+  it("hasValue", () => {
+    for (let i = 1; i <= 26; i++) {
+      expect(map.hasValue(i)).toBe(true); // A-Z
+      expect(map.hasValue(i + 100)).toBe(false); // a-z
+    }
+  });
+});
+
+describe("conflict scenarios", () => {
+  it("{A => 1} followed by {A => 2} results in {A => 1, A => 2}", () => {
+    const map = new AssociationTable<string, number>();
+    map.associate("A", 1);
+    map.associate("A", 2);
+    expect(map.get("A")).toMatchObject([1, 2]);
+    expect(map.getKey(1)).toMatchObject(["A"]);
+    expect(map.getKey(2)).toMatchObject(["A"]);
+    expect(map.size).toBe(2);
+  });
+  it("{A => 1} followed by {B => 1} results in {A => 1, B => 1} and getKey(1) = [A, B]", () => {
+    const map = new AssociationTable<string, number>();
+    map.associate("A", 1);
+    map.associate("B", 1);
+    expect(map.get("A")).toMatchObject([1]);
+    expect(map.get("B")).toMatchObject([1]);
+    expect(map.getKey(1)).toMatchObject(["A", "B"]);
+    expect(map.size).toBe(2);
+  });
+  it("deleting 1 from {A => 1, B => 1} results in empty set", () => {
+    const map = new AssociationTable<string, number>();
+    map.associate("A", 1);
+    map.associate("B", 1);
+    map.deleteValue(1);
+    expect(map.get("A")).toMatchObject([]);
+    expect(map.get("B")).toMatchObject([]);
+    expect(map.getKey(1)).toMatchObject([]);
+    expect(map.size).toBe(0);
+  });
+  it("deleting A from {A => 1, B => 1} results in {B => 1}", () => {
+    const map = new AssociationTable<string, number>();
+    map.associate("A", 1);
+    map.associate("B", 1);
+    map.delete("A");
+    expect(map.get("A")).toMatchObject([]);
+    expect(map.get("B")).toMatchObject([1]);
+    expect(map.getKey(1)).toMatchObject(["B"]);
+    expect(map.size).toBe(1);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,132 @@
+export interface IAssociationTable<K, V> {
+  readonly size: number; // returns the total number of associations
+  get: (key: K) => V[]; // returns associated values
+  getKey: (value: V) => K[]; // returns associated keys
+  getValue: (key: K) => V[]; // returns associated values
+  associate: (key: K, value: V) => this; // adds an association between key and value
+  clear: () => void; // resets the association table
+  delete: (key: K) => boolean; // Deletes all associations related to the given key. Returns true if any associations existed and were been removed, or false if none were found.
+  deleteKey: (key: K) => boolean; // Deletes all associations related to the given key. Returns true if any associations existed and were been removed, or false if none were found
+  deleteValue: (value: V) => boolean; // Deletes all associations related to the given value. Returns true if any associations existed and were been removed, or false if none were found
+  forEach: (
+    callbackfn: (value: V, key: K, table: IAssociationTable<K, V>) => void,
+    thisArg?: any
+  ) => void; // executes the provided callback once for each association
+  has: (key: K) => boolean; // Returns true if any associations with the specified key exists; otherwise false.
+  hasKey: (key: K) => boolean; // Returns true if any associations with the specified key exists; otherwise false.
+  hasValue: (value: V) => boolean; // Returns true if any associations with the specified value exists; otherwise false.
+  [Symbol.toStringTag]: "AssociationTable"; // used in util.inspect / console.log
+  inspect: () => string; // A utility function to inspect current contents as a string
+}
+
+class AssociationTable<K, V> implements IAssociationTable<K, V> {
+  public [Symbol.toStringTag]: "AssociationTable";
+
+  private associationsKToV: Map<K, V[]> = new Map<K, V[]>();
+  private associationsVToK: Map<V, K[]> = new Map<V, K[]>();
+  private numberOfAssociations = 0;
+
+  get size() {
+    return this.numberOfAssociations / 2; // number of associations should always be divisible by two
+  }
+  public get = (key: K) => Array.from(this.associationsKToV.get(key) || []); // Array.from used to keep the Map's array internal, avoiding possibility an array modification after API call
+  public getKey = (value: V) =>
+    Array.from(this.associationsVToK.get(value) || []); // Array.from used to keep the Map's array internal, avoiding possibility an array modification after API call
+  public getValue = (key: K) => this.get(key);
+  public associate = (key: K, value: V) => {
+    let keyArray: K[] | undefined = this.associationsVToK.get(value);
+    let valueArray: V[] | undefined = this.associationsKToV.get(key);
+
+    if (!keyArray) {
+      keyArray = [];
+      this.associationsVToK.set(value, keyArray);
+    }
+    if (!valueArray) {
+      valueArray = [];
+      this.associationsKToV.set(key, valueArray);
+    }
+
+    if (!keyArray.includes(key)) {
+      keyArray.push(key);
+      this.numberOfAssociations += 1;
+    }
+    if (!valueArray.includes(value)) {
+      valueArray.push(value);
+      this.numberOfAssociations += 1;
+    }
+    return this;
+  };
+  public clear = () => {
+    this.associationsKToV.clear();
+    this.associationsVToK.clear();
+    this.numberOfAssociations = 0;
+  };
+  public delete = (key: K) => {
+    const valueArray: V[] | undefined = this.associationsKToV.get(key);
+
+    if (!valueArray) {
+      return false;
+    }
+    valueArray.forEach(value => {
+      const keyArray = this.associationsVToK.get(value) as K[];
+      this.numberOfAssociations -= 1;
+      keyArray.splice(keyArray.indexOf(key), 1);
+      if (keyArray.length === 0) {
+        this.associationsVToK.delete(value);
+      }
+    });
+    this.numberOfAssociations -= valueArray.length;
+    this.associationsKToV.delete(key);
+    return true;
+  };
+  public deleteKey = (key: K) => this.delete(key);
+  public deleteValue = (value: V) => {
+    const keyArray: K[] | undefined = this.associationsVToK.get(value);
+
+    if (!keyArray) {
+      return false;
+    }
+    keyArray.forEach(key => {
+      const valueArray = this.associationsKToV.get(key) as V[];
+      this.numberOfAssociations -= 1;
+      valueArray.splice(valueArray.indexOf(value));
+      if (valueArray.length === 0) {
+        this.associationsKToV.delete(key);
+      }
+    });
+    this.numberOfAssociations -= keyArray.length;
+    this.associationsVToK.delete(value);
+    return true;
+  };
+  public forEach = (
+    callbackfn: (value: V, key: K, table: IAssociationTable<K, V>) => void,
+    thisArg?: any
+  ) => {
+    this.associationsKToV.forEach((valueArray, key) => {
+      valueArray.forEach(value => {
+        callbackfn.apply(thisArg, [value, key, this]);
+      });
+    });
+  };
+  public has = (key: K) => this.associationsKToV.has(key);
+  public hasKey = (key: K) => this.has(key);
+  public hasValue = (value: V) => this.associationsVToK.has(value);
+  public hasAssociation(key: K, value: V) {
+    this.get(key).includes(value);
+  }
+  public inspect = () => {
+    let str = "AssociationTable {";
+    let entry = 0;
+    this.forEach((value, key) => {
+      entry++;
+      str += "" + key.toString() + " => " + value.toString() + "";
+      if (entry < this.size) {
+        str += ", ";
+      }
+    });
+    str += "}";
+    return str;
+  };
+}
+
+export default AssociationTable;
